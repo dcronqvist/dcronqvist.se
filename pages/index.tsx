a517ad600035a2c58d203761804f5c06ab9ac603
Daniel Cronqvist <dancro99@gmail.com>
2021-07-31 15:42:12 +0200

    Fixed image to be normal img tag, and added export command to buil

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ const IndexPage = ({ articlesData }: IndexPageProps) => {
       </div>
       <div className={`${theme.index.profilepic} ${theme.index.third}`}>
         <Tooltipped text="Yep, that's me!">
-          <Image quality={100} src={profilePic}/>
+          <img style={{width: "100%"}} src={"imgofme.png"}></img>
         </Tooltipped>
       </div>   
     </div>

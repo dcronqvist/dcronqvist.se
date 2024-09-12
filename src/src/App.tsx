556cc84a5e87f091d516c711d8a28ad112bfc211
Daniel Cronqvist <dancro99@gmail.com>
2024-09-12 20:30:01 +0200

    Some new stuff

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,25 +1,121 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import styled from 'styled-components';
+
+function Separator() {
+  const Separator = styled.hr`
+    border: 1px solid lightblue;
+    padding: 0px;
+    margin: 0px;
+    `;
+
+  return <Separator />;
+}
+
+function Header() {
+  const HeaderDiv = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  `;
+
+  return (
+    <header>
+      <HeaderDiv>
+        <p>dcronqvist</p>
+        <p>github</p>
+        <p>linkedin</p>
+        <p>email</p>
+      </HeaderDiv>
+      <Separator />
+    </header>
+  );
+}
+
+function Sidebar() {
+  const SidebarDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    & > p,h3 {
+      margin-left: 20px;
+    }
+  `;
+
+  return (
+    <SidebarDiv>
+      <p>search...</p>
+      <h3>tags</h3>
+      <p>react</p>
+      <p>javascript</p>
+      <p>c#</p>
+    </SidebarDiv>
+  );
+};
+
+function ScrollableContent() {
+  return (
+    <div>
+      <p>scrollable content</p>
+      <p>scrollable content</p>
+    </div>
+  );
+}
+
+function Content() {
+  const Grid = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+
+    border: 1px solid green;
+
+    & > div {
+      border: 1px solid blue;
+    }
+
+    :first-child {
+      width: 30%;
+    }
+
+    :nth-child(2) {
+      width: 70%;
+    }
+  `;
+
+  return (
+    <Grid>
+      <Sidebar />
+      <ScrollableContent />
+    </Grid>
+  );
+}
 
 function App() {
+  const RootWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #282c34;
+    color: lightgray;
+  `;
+
+  const ContentDiv = styled.div`
+    width: 800px;
+    border: 1px solid red;
+    height: 90%;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RootWrapper>
+      <ContentDiv>
+        <Header />
+        <Content />
+      </ContentDiv>
+    </RootWrapper>
   );
 }
 
